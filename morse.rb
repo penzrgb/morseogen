28f97d5653a9ccda39e6b69ad34568eb9ecdd40c
Rodger Benham <rodger.benham@fridaymedia.com.au>
2014-08-10 16:15:33 +1000

    Duration calculates correctly for single characters

--- morse.rb
+++ morse.rb
@@ -1,8 +1,17 @@
 class Morse
   attr_accessor :code, :duration
 
-  def initialize(code, duration)
-    @code = code
-    @duration = duration
+  def initialize(options = {})
+    @code = options[:code]
+    @duration = options[:duration] || calculate_duration(@code)
+  end
+
+  def calculate_duration(str)
+    result = 0
+    str.each_char do |c|
+      result += 1 if c == " " || c == "·"
+      result += 3 if c == "—"
+    end
+    result
   end
 end
--- spec/morse_generator_spec.rb
+++ spec/morse_generator_spec.rb
@@ -7,6 +7,16 @@ describe MorseGenerator do
       expect(MorseGenerator.generate "TEST").to be_a Morse
     end
 
+    it "generates correct morse code for A" do
+      m = MorseGenerator.generate "A"
+      expect(m.code).to eq "· —"
+    end
+
+    it "generates correct duration for A" do
+      m = MorseGenerator.generate "A"
+      expect(m.duration).to eq 5
+    end
+
     it "generates correct morse code for SOS" do
       m = MorseGenerator.generate "SOS"
       expect(m.code).to eq "· · ·   — — —   · · ·       "
